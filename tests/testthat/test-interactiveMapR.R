de035c58cdbb79b7892d8db268da5fb389d84850
jbdorey <69567713+jbdorey@users.noreply.github.com>
2023-06-27 19:13:17 +0930

    Update test-interactiveMapR.R

--- tests/testthat/test-interactiveMapR.R
+++ tests/testthat/test-interactiveMapR.R
@@ -30,4 +30,41 @@ interactiveMapR(
 )
 
 
+# Check directory that the plot was saved
+testthat::test_that("interactiveMapR plot saved?", {
+  testthat::expect_true(any(stringr::str_detect(list.files(tempdir()), "interactiveMaps")))
+})
+# Check directory that the plot was saved
+testthat::test_that("interactiveMapR plot saved?", {
+  testthat::expect_equal(length(list.files(paste0(tempdir(), "/interactiveMaps"))), 3)
+})
+
+  # Find the .html files that were made
+directories <- list.files(paste0(tempdir(), "/interactiveMaps"), 
+                         full.names = TRUE)
+  # Read in the html files for each species
+A_tyleri <- xml2::read_html(directories[stringr::str_detect(directories, "tyleri")])
+C_rhodopus <- xml2::read_html(directories[stringr::str_detect(directories, "rhodopus")])
+P_octomaculata <- xml2::read_html(directories[stringr::str_detect(directories, "octomaculata")])
+
+# Check directory that the plot was saved
+testthat::test_that("interactiveMapR detect Agapostemon tyleri Cockerell, 1917", {
+  testthat::expect_true(stringr::str_detect(A_tyleri %>% as.character(), 
+                                            "Agapostemon tyleri Cockerell, 1917"))
+})
+testthat::test_that("interactiveMapR detect Centris rhodopus Cockerell, 1897", {
+  testthat::expect_true(stringr::str_detect(C_rhodopus %>% as.character(), 
+                                            "Centris rhodopus Cockerell, 1897"))
+})
+testthat::test_that("interactiveMapR detect Perdita octomaculata (Say, 1824)", {
+  testthat::expect_true(stringr::str_detect(P_octomaculata %>% as.character(), 
+                                            "Perdita octomaculata \\(Say, 1824\\)"))
+})
+
+
+# Test class
+testthat::test_that("interactiveMapR expected class", {
+  testthat::expect_equal(attributes(A_tyleri)$class, c("xml_document", "xml_node" ))
+})
+
 
