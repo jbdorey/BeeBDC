267e4132d596ac32f6c7e9c6a5985e690e00f4c7
Erica Fischer <54035657+efishy@users.noreply.github.com>
2023-06-27 11:22:48 +0100

    Add files via upload
    Batch uploading all my completed test scripts so far

---
+++ tests/testthat/test-coordUncerFlagR.R
@@ -0,0 +1,47 @@
+requireNamespace("readr")
+requireNamespace("tibble")
+
+
+# make us some data - this is FAKE DATA derived from records from SCAN
+testData <- tibble::tribble(
+                   ~basisOfRecord,   ~kingdom,      ~phylum,    ~class,        ~order,  ~family,  ~scientificName, ~taxonID, ~scientificNameAuthorship, ~genus, ~specificEpithet, ~recordedBy, ~eventDate, ~year, ~month, ~day,        ~verbatimEventDate,        ~country, ~stateProvince, ~locality,                  ~locationRemarks, ~decimalLatitude, ~decimalLongitude, ~coordinateUncertaintyInMeters,                                             ~rights,                                     ~rightsHolder,
+              "PreservedSpecimen", "Animalia", "Arthropoda", "Insecta", "Hymenoptera", "apidae", "apis mellifera",  235783L,          "Linnaeus, 1758", "Apis",      "mellifera", "P.S. Ward",  "6/28/05", 2005L,     6L,  28L, "28 Jun 2005/29 Jun 2005", "United States",   "California",   "Davis", "coordinates obtained from Label",            38.54,        -121.75667,                           200L, "http://creativecommons.org/publicdomain/zero/1.0/", "The California Academy of Sciences - AntWeb.org",
+              "PreservedSpecimen", "Animalia", "Arthropoda", "Insecta", "Hymenoptera", "apidae", "apis mellifera",  235783L,          "Linnaeus, 1758", "Apis",      "mellifera", "P.S. Ward",  "6/28/05", 2005L,     6L,  28L, "28 Jun 2005/29 Jun 2005", "United States",   "California",   "Davis", "coordinates obtained from Label",            38.54,        -121.75667,                            10L, "http://creativecommons.org/publicdomain/zero/1.0/", "The California Academy of Sciences - AntWeb.org",
+              "PreservedSpecimen", "Animalia", "Arthropoda", "Insecta", "Hymenoptera", "apidae", "apis mellifera",  235783L,          "Linnaeus, 1758", "Apis",      "mellifera", "P.S. Ward",  "6/28/05", 2005L,     6L,  28L, "28 Jun 2005/29 Jun 2005", "United States",   "California",   "Davis", "coordinates obtained from Label",            38.54,        -121.75667,                           500L, "http://creativecommons.org/publicdomain/zero/1.0/", "The California Academy of Sciences - AntWeb.org",
+              "PreservedSpecimen", "Animalia", "Arthropoda", "Insecta", "Hymenoptera", "apidae", "apis mellifera",  235783L,          "Linnaeus, 1758", "Apis",      "mellifera", "P.S. Ward",  "6/28/05", 2005L,     6L,  28L, "28 Jun 2005/29 Jun 2005", "United States",   "California",   "Davis", "coordinates obtained from Label",            38.54,        -121.75667,                          1000L, "http://creativecommons.org/publicdomain/zero/1.0/", "The California Academy of Sciences - AntWeb.org",
+              "PreservedSpecimen", "Animalia", "Arthropoda", "Insecta", "Hymenoptera", "apidae", "apis mellifera",  235783L,          "Linnaeus, 1758", "Apis",      "mellifera", "P.S. Ward",  "6/28/05", 2005L,     6L,  28L, "28 Jun 2005/29 Jun 2005", "United States",   "California",   "Davis", "coordinates obtained from Label",            38.54,        -121.75667,                           850L, "http://creativecommons.org/publicdomain/zero/1.0/", "The California Academy of Sciences - AntWeb.org",
+              "PreservedSpecimen", "Animalia", "Arthropoda", "Insecta", "Hymenoptera", "apidae", "apis mellifera",  235783L,          "Linnaeus, 1758", "Apis",      "mellifera", "P.S. Ward",  "6/28/05", 2005L,     6L,  28L, "28 Jun 2005/29 Jun 2005", "United States",   "California",   "Davis", "coordinates obtained from Label",            38.54,        -121.75667,                           600L, "http://creativecommons.org/publicdomain/zero/1.0/", "The California Academy of Sciences - AntWeb.org",
+              "PreservedSpecimen", "Animalia", "Arthropoda", "Insecta", "Hymenoptera", "apidae", "apis mellifera",  235783L,          "Linnaeus, 1758", "Apis",      "mellifera", "P.S. Ward",  "6/28/05", 2005L,     6L,  28L, "28 Jun 2005/29 Jun 2005", "United States",   "California",   "Davis", "coordinates obtained from Label",            38.54,        -121.75667,                           150L, "http://creativecommons.org/publicdomain/zero/1.0/", "The California Academy of Sciences - AntWeb.org",
+              "PreservedSpecimen", "Animalia", "Arthropoda", "Insecta", "Hymenoptera", "apidae", "apis mellifera",  235783L,          "Linnaeus, 1758", "Apis",      "mellifera", "P.S. Ward",  "6/28/05", 2005L,     6L,  28L, "28 Jun 2005/29 Jun 2005", "United States",   "California",   "Davis", "coordinates obtained from Label",            38.54,        -121.75667,                            20L, "http://creativecommons.org/publicdomain/zero/1.0/", "The California Academy of Sciences - AntWeb.org"
+              )
+
+
+# run the function
+testOut <- BeeDC::coordUncerFlagR(data = testData, uncerColumn = "coordinateUncertaintyInMeters", threshold = 500)
+
+
+# number of FALSE values in new column, .uncertaintyThreshold
+resultsT <- length(testOut$.uncertaintyThreshold[testOut$.uncertaintyThreshold == TRUE])
+resultsF <- length(testOut$.uncertaintyThreshold[testOut$.uncertaintyThreshold == FALSE])
+
+
+# number of columns in old and new data
+old <- ncol(testData)
+new <- ncol(testOut)
+testcols <- new - old
+
+
+# Test the number of expected TRUE and FALSE values in the .uncertaintyThreshold column and
+# verify addition of new column
+testthat::test_that("coordUncerFlagR results columns TRUE", {
+  testthat::expect_equal(resultsT, 5)
+})
+
+testthat::test_that("coordUncerFlagR results columns FALSE", {
+  testthat::expect_equal(resultsF, 3)
+})
+
+testthat::test_that("verify addition of new column", {
+  testthat::expect_equal(testcols, 1)
+})
+
---
+++ tests/testthat/test-countryNameCleanR.R
@@ -0,0 +1,55 @@
+requireNamespace("readr")
+requireNamespace("tibble")
+library(dplyr) ## could not use %>% without loading as library
+
+
+# make yourself some test data which has common problem names for the USA and one row that is already correct
+testData <- tibble::tribble(
+              ~database_id, ~decimalLatitude, ~decimalLongitude,                  ~country, ~countryCode,                 ~scientificName, ~dataSource, ~recordedBy,        ~order,  ~family, ~genus, ~specificEpithet, ~scientificNameAuthorship,
+               "fake SCAN",            38.54,        -121.75667,           "United States",           NA, "Apis mellifera Linneaus, 1758",      "fake",          NA, "Hymenoptera", "apidae", "Apis",      "mellifera",          "Linnaeus, 1758",
+               "fake SCAN",            38.54,        -121.75667,                  "U.S.A.",           NA, "Apis mellifera Linneaus, 1759",      "fake",          NA, "Hymenoptera", "apidae", "Apis",      "mellifera",          "Linnaeus, 1758",
+               "fake SCAN",            38.54,        -121.75667,                  "U.S.A.",           NA, "Apis mellifera Linneaus, 1760",      "fake",          NA, "Hymenoptera", "apidae", "Apis",      "mellifera",          "Linnaeus, 1758",
+               "fake SCAN",            38.54,        -121.75667,                      "US",           NA, "Apis mellifera Linneaus, 1761",      "fake",          NA, "Hymenoptera", "apidae", "Apis",      "mellifera",          "Linnaeus, 1758",
+               "fake SCAN",            38.54,        -121.75667,                     "USA",           NA, "Apis mellifera Linneaus, 1762",      "fake",          NA, "Hymenoptera", "apidae", "Apis",      "mellifera",          "Linnaeus, 1758",
+               "fake SCAN",            38.54,        -121.75667,                     "usa",           NA, "Apis mellifera Linneaus, 1763",      "fake",          NA, "Hymenoptera", "apidae", "Apis",      "mellifera",          "Linnaeus, 1758",
+               "fake SCAN",            38.54,        -121.75667,           "UNITED STATES",           NA, "Apis mellifera Linneaus, 1764",      "fake",          NA, "Hymenoptera", "apidae", "Apis",      "mellifera",          "Linnaeus, 1758",
+               "fake SCAN",            38.54,        -121.75667,"United States of America",           NA, "Apis mellifera Linneaus, 1758",      "fake",          NA, "Hymenoptera", "apidae", "Apis",      "mellifera",          "Linnaeus, 1758",
+               "fake SCAN",            38.54,        -121.75667,                     "usa",           NA, "Apis mellifera Linneaus, 1765",      "fake",          NA, "Hymenoptera", "apidae", "Apis",      "mellifera",          "Linnaeus, 1758"
+              )
+
+# common problem table from the function documentation
+commonProblems <- tibble::tibble(problem = c('U.S.A.', 'US','USA','usa','UNITED STATES',
+                                            'United States','U.S.A','MX','CA','Bras.','Braz.',
+                                            'Brasil','CNMI','USA TERRITORY: PUERTO RICO'),
+                                 fix = c('United States of America','United States of America',
+                                        'United States of America','United States of America',
+                                        'United States of America','United States of America',
+                                        'United States of America','Mexico','Canada','Brazil',
+                                        'Brazil','Brazil','Northern Mariana Islands','PUERTO.RICO'))
+
+
+# run the function!
+testOut <- BeeDC::countryNameCleanR(data = testData, commonProblems = commonProblems)
+
+
+# check that country column has only one value (correct value)
+numberOfCountries <- testOut %>% group_by(country) %>% summarise()
+
+
+# number of NA values in fix column
+numberNAs <- sum(is.na(testOut$fix))
+
+
+# Test the number of expected fixed and NA values in the fix column and
+# verify addition of new column
+testthat::test_that("number of country names in the fixed country column" , {
+  testthat::expect_equal(ncol(numberOfCountries), 1)
+})
+
+testthat::test_that("number of unchanged rows in data", {
+  testthat::expect_equal(numberNAs, 1)
+})
+
+
+
+
