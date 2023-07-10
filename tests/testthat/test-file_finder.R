e1da41af648484f0c21dc5796ec3efb7ab3e8730
Erica Fischer <54035657+efishy@users.noreply.github.com>
2023-07-10 11:21:12 +0100

    Add files via upload

---
+++ tests/testthat/test-file_finder.R
@@ -0,0 +1,32 @@
+requireNamespace("readr")
+requireNamespace("BeeDC")
+requireNamespace("dplyr")
+requireNamespace("openxlsx")
+
+library(dplyr) ## could not use %>% without loading as library
+
+
+## fake data
+testData <- dplyr::tribble(
+  ~catalog_number, ~pollinator_family, ~pollinator_genus, ~pollinator_species, ~collection_method, ~collector_number, ~day_collected, ~month_collected, ~year_collected,                   ~location_description, ~location_name,    ~habitat, ~latitude,  ~longitude, ~basis_of_record,
+  "SFU7052435916",       "Halictidae",    "Secret",             "sp. 1",    "Pantrap, blue",               20L,            30L,            "Apr",           2007L, "Bear Hill Regional Park, Victoria CRD",           "BH", "Garry Oak", 48.545605, -123.406769,       "specimen",
+  "SFU705917",       "Andrenidae",         "Secret",     "sp.",    "Pantrap, blue",               20L,            30L,            "Apr",           2007L, "Bear Hill Regional Park, Victoria CRD",           "BH", "Garry Oak", 48.545605, -123.406769,       "specimen",
+  "SFU7052346919",       "Halictidae",    "Secret",       "sp.",    "Pantrap, blue",               20L,            30L,            "Apr",           2007L, "Bear Hill Regional Park, Victoria CRD",           "BH", "Garry Oak", 48.545605, -123.406769,       "specimen",
+  "SFU7052645920",       "Andrenidae",         "Secret",     "sp.",    "Pantrap, blue",               20L,            30L,            "Apr",           2007L, "Bear Hill Regional Park, Victoria CRD",           "BH", "Garry Oak", 48.545605, -123.406769,       "specimen",
+  "SFU705645921",       "Andrenidae",         "Secret",     "sp.",    "Pantrap, blue",               20L,            30L,            "Apr",           2007L, "Bear Hill Regional Park, Victoria CRD",           "BH", "Garry Oak", 48.545605, -123.406769,       "specimen"
+)
+
+
+## write file
+readr::write_csv(testData, paste0(tempdir(), "/testData.csv"))
+
+
+## find file 
+testOut <- BeeDC::file_finder(path = paste0(tempdir()), fileName = "testData.csv")
+
+
+## test
+testthat::test_that("file_finder expected class", {
+  testthat::expect_type(testOut, "character")
+})
+
