c8e703cfb8c296d9fe04bc0100c2f983daf375da
jbdorey <69567713+jbdorey@users.noreply.github.com>
2023-09-20 14:04:22 +0930

    Update test-beesTaxonomy.R

--- tests/testthat/test-beesTaxonomy.R
+++ tests/testthat/test-beesTaxonomy.R
@@ -31,25 +31,25 @@ testthat::test_that("testTaxonomy expected class", {
 # load in the full dataset
 taxonomyFile <- BeeBDC::beesTaxonomy()
 
-if(!is.null(beesTaxonomy)){
+if(!is.null(taxonomyFile)){
 # Test the expected results
 testthat::test_that("beesTaxonomy expected number of columns", {
-  testthat::expect_equal(ncol(beesTaxonomy), 24)
+  testthat::expect_equal(ncol(taxonomyFile), 24)
 })
 testthat::test_that("beesTaxonomy expect that number of unique ids == number of overall rows", {
-  testthat::expect_equal(length(unique(beesTaxonomy$id)), nrow(beesTaxonomy))
+  testthat::expect_equal(length(unique(taxonomyFile$id)), nrow(taxonomyFile))
 })
 testthat::test_that("beesTaxonomy expect that number of 0[accid] == accepted[taxostatus]", {
-  testthat::expect_equal(sum(beesTaxonomy$accid == 0), 
-                         sum(beesTaxonomy$taxonomic_status == "accepted"))
+  testthat::expect_equal(sum(taxonomyFile$accid == 0), 
+                         sum(taxonomyFile$taxonomic_status == "accepted"))
 })
 
 # Test classes
 testthat::test_that("beesTaxonomy expected class", {
-  testthat::expect_type(beesTaxonomy, "list")
+  testthat::expect_type(taxonomyFile, "list")
 })
 testthat::test_that("beesTaxonomy expected class", {
-  testthat::expect_equal(attributes(beesTaxonomy)$class, c("spec_tbl_df", "tbl_df","tbl","data.frame" ))
+  testthat::expect_equal(attributes(taxonomyFile)$class, c("spec_tbl_df", "tbl_df","tbl","data.frame" ))
 })
 
 } #END !is.null
