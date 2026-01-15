4ebeefe1880c199b71b4f7cfe00404eac0e67db4
jbdorey <jbdorey@me.com>
2026-01-15 16:08:45 +1100

    testing

--- tests/testthat/test-beesChecklist.R
+++ tests/testthat/test-beesChecklist.R
@@ -18,25 +18,20 @@ testthat::test_that("testChecklist expected class", {
   testthat::expect_equal(attributes(testChecklist)$class, c("tbl_df","tbl","data.frame" ))
 })
 
-
 # Define download headers
 # headers <- c(Authorization = paste("token", "20271361303ce042ff9cce49ecd9c8f23594ed4032e83f150e535dbd2b67297ea88448e3ca29260f6c416f581834094a898089d90f2229111845f01ef8b75f46"))
 # # Set some options for accessing the data
-# options(timeout=400,
-#         HTTPUserAgent = paste0(names(headers), " ", headers, collapse = " ", sep = " "))
 
-withr::local_options(list(
-  HTTPUserAgent = sprintf("BeeBDC-tests/%s (R/%s.%s)",
-                          as.character(utils::packageVersion("BeeBDC")),
-                          R.version$major, R.version$minor)  ))
+options(timeout=400,
+        HTTPUserAgent = sprintf("BeeBDC-tests/%s (R/%s.%s)",
+                                as.character(utils::packageVersion("BeeBDC")),
+                                R.version$major, R.version$minor)  )
 token <- "20271361303ce042ff9cce49ecd9c8f23594ed4032e83f150e535dbd2b67297ea88448e3ca29260f6c416f581834094a898089d90f2229111845f01ef8b75f46"
 headers <- c("User-Agent" = sprintf("BeeBDC-tests/%s (R/%s.%s)",
                                     as.character(utils::packageVersion("BeeBDC")),
                                     R.version$major, R.version$minor),
              "Authorization" = sprintf("token %s", token))
 
-
-# TEST the full data
 
 OS <- dplyr::if_else(.Platform$OS.type == "unix",
                      "MacLinux",
@@ -48,12 +43,8 @@ if(OS == "Windows"){
 }
 
 beesChecklist <- NULL
-file_id = "60945823L"
-destfile = file.path(tempdir())
-url_nd <- sprintf("http://open.flinders.edu.au/ndownloader/files/%s", file_id)
 # load in the full dataset
-beesChecklist <- BeeBDC::beesChecklist(URL = ,
-                                       mode = mode,
+beesChecklist <- BeeBDC::beesChecklist(mode = mode,
                                        headers = headers)
 
 if(!is.null(beesChecklist)){
